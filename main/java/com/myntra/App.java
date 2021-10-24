f38c938be00687ec72d75597053625f3feafb44e
suman <suman21.ray@gmail.com>
2021-10-24 14:37:16 +0530

    first commit

---
+++ main/java/com/myntra/App.java
@@ -0,0 +1,13 @@
+package com.myntra;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
